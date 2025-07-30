60ca4ca8afffd09bd80e37a0e940a731a5e5b97f
Nimely111 <samuelehnimely@gmail.com>
2025-07-30 19:35:12 +0000

    variable and data types challenge solved

---
+++ variable-practice.js
@@ -0,0 +1,38 @@
+// TODO: Declare variables using appropriate naming conventions
+
+// 1. Create variables for a student using camelCase:
+//    - firstName (string)
+//    - lastName (string) 
+//    - studentAge (number)
+//    - isEnrolled (boolean)
+
+let firstName = "John";
+let lastName = "Doe";
+let studentAge = 20;
+let isEnrolled = true;
+
+console.log("student: ", firstName, lastName, "Age ", studentAge, "Enrolled: ", isEnrolled)
+
+// 2. Create variables for course info using snake_case:
+//    - course_name (string)
+//    - course_duration (number)
+//    - max_students (number)
+let course_name = "Web Development";
+let course_duration = 12;
+let max_students = 30;
+
+console.log("Course: ", course_name, "Duration: ", course_duration, "Weeks", "Max Students: ", max_students)
+
+// 3. Create constants for university info:
+//    - UNIVERSITY_NAME (string)
+//    - FOUNDED_YEAR (number)
+const UNIVERSITY_NAME = "Starz university";
+const FOUNDED_YEAR = 2010;
+
+console.log("University: ", UNIVERSITY_NAME, "Founded: ", FOUNDED_YEAR);
+
+// 4. Practice undefined and null:
+//    - Create a variable 'grade' that is undefined
+//    - Create a variable 'previousScore' that is null
+
+// 5. Use console.log to display all variables and their types
