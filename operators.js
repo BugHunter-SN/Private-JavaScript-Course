49c9965b22bf7709807fafa3aae5aca66b702cbd
Nimely111 <samuelehnimely@gmail.com>
2025-08-06 22:24:11 +0000

    update: add new file

---
+++ operators.js
@@ -0,0 +1,23 @@
+// Arithmetic Operators
+let num1 = 10;
+let num2 = 3;
+
+console.log("Result: "+ (num1 + num2)); // 13 (Addition)
+console.log(num1 - num2); // 7  (Subtraction)
+console.log(num1 * num2); // 30 (Multiplication)
+console.log(num1 / num2); // 3.333... (Division)
+console.log(Math.floor(num1 / num2)); // 3.333... (Division)
+console.log(num1 % num2); // 1  (Modulus - remainder)
+console.log(num1 ** num2); // 1000 (Exponentiation) 
+
+
+// Assignment Operators
+let x = 10;
+
+// x = x + 5 // longer way
+console.log(x += 5) // x plus or equal to 5 or add and assign
+
+console.log(x -= 3) // x = x - 3 (now 12)
+
+x *= 2; // x = x * 2 (now 24)
+x /= 4; // x = x / 4 (now 6)
