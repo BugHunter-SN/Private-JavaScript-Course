4dfc373d206a334b4120515045403c86e87ea5b4
Nimely111 <samuelehnimely@gmail.com>
2025-08-13 22:18:51 +0000

    modified

---
+++ string.js
@@ -0,0 +1,6 @@
+const firstName = "John";
+const lastName = "Doe";
+const age = 30;
+// My name is John Doe and I am 30 years of age.
+
+console.log("My name is " + firstName + " " + lastName + " "+ "and I am " + age + " years of age")
