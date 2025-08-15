4098595d354d63751fcf96e8b236766d5b8a450c
Nimely111 <samuelehnimely@gmail.com>
2025-08-15 12:01:58 +0000

    updated

--- string.js
+++ string.js
@@ -1,6 +1,25 @@
-const firstName = "John";
-const lastName = "Doe";
-const age = 30;
-// My name is John Doe and I am 30 years of age.
+// const firstName = "John";
+// const lastName = "Doe";
+// const age = 30;
+// // My name is John Doe and I am 30 years of age.
 
-console.log("My name is " + firstName + " " + lastName + " "+ "and I am " + age + " years of age")
+// // old way - string concatenation
+// console.log("My name is " + firstName + " " + lastName + " "+ "and I am " + age + " years of age")
+
+
+// // modern way -  template literals ES2015/ES6
+// console.log(`My name is ${firstName} ${lastName} and I am $`{age} years of age`)
+
+const fullName = "Samuel Nimely";
+
+// Properties
+console.log(fullName.length); // 11
+
+// Methods
+console.log(fullName.substring(0,3));
+console.log(fullName.toUpperCase()); // "SAMUEL NIMELY"
+console.log(fullName.toLowerCase()); // "samuel nimely"
+console.log(fullName.split(" ")); // ['Samuel', 'Nimely']
+console.log(fullName.split("")); //  ['S', 'a', 'm', 'u', 'e', 'l', ' ', 'N', 'i', 'm', 'e', 'l', 'y']
+// console.log(fullName.indexOf("N"))
+console.log(fullName.length -2) // 11
