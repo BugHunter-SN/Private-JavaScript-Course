2b9f70f218f16481d061417f3b03ab027da5d62f
Nimely111 <samuelehnimely@gmail.com>
2025-09-05 23:01:56 +0000

    feat: add files for DOM introduction

--- function.js
+++ function.js
@@ -1,7 +1,44 @@
-// function greetings(name){
-//     return "Hello " + name;
-    
+// greeting  = "Hello";
+// userName = "world";
+
+// function greet(greeting = "Hello", userName = "Peter"){
+//     return greeting, userName
 // }
 
-// const result = greetings("James");
-// console.log(result);
+// const result = greet('Hi', 'John')
+// const result1 = greet('Good Morning', 'James')
+// const result2 = greet('Hi', 'Esther')
+// console.log(result, result1, result2)
+
+function add(x, y){
+    return x + y
+}
+const sum = add(2,3)
+// console.log(sum)
+
+function multiply(x, y){
+    return x * y 
+}
+const product = multiply(5,3)
+// console.log(product)
+
+
+
+
+
+const greetings = (greeting = "Hello", name) => {
+  if (!name) {
+    return greeting;
+  } else {
+    return `${greeting} ${name}`;
+  }
+}
+
+console.log(greetings("Hi")); // "Hi"
+console.log(greetings("Hi", "John")); // "Hi John"
+
+
+let adding = (x, y) =>  x + y;
+console.log(adding(3,3))
+console.log(adding(10,3))
+console.log(adding(10,30))
